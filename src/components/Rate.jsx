f4696a188ac05f516392b2e995d98092b26e1415
skierua <iurii.vasurchak@gmail.com>
2025-08-09 11:56:27 +0300

    social rebuild

--- src/components/Rate.jsx
+++ src/components/Rate.jsx
@@ -38,12 +38,18 @@ export const Rate = (props) => {
     fedit,
     fisedited,
     frefresh,
-    // fpublish, // publish rates
+    fpublish, // publish rates
     ...other
   } = props;
   const [knt, setKnt] = useState(pl.term);
 
-  // console.log(cur);
+  // console.log(
+  //   sqldata.filter((v) => (knt === "" || knt === v.shop) && v.prc === "bulk")
+  // );
+  // console.log(
+  //   sqldata.filter((v) => (knt === "" || knt === v.shop) && v.prc !== "bulk")
+  // );
+
   const crossRate = (sub = "") => {
     let ret = [];
     let c1 = "",
@@ -86,23 +92,28 @@ export const Rate = (props) => {
 
   const onPbl_clicked = () => {
     console.log("knt=" + knt);
+
     const rate = (v) => {
       return v === "" ? "--.--" : Number(v).toPrecision(4); //Number(v).toFixed(2);
     };
+
     const qty = (q) => {
       return q === "1" ? "" : `${q} `;
     };
+
     const cursub = (v, cur, shop) => {
       if (cur !== USDCHAR || shop !== kntBulk) return "";
       if (v === "20") return "білий";
       else if (v === "50") return "шмельц";
       return "синій";
     };
+
     const flag = (v) => {
       if (v === "USD") return "🇺🇸";
       if (v === "EUR") return "🇪🇺";
       if (v === "PLN") return "🇲🇨";
       if (v === "CZK") return "🇨🇿";
+      if (v === "GBP") return "🇬🇧";
       return v; // for other currencies
     };
     let res = "";
@@ -175,6 +186,118 @@ export const Rate = (props) => {
     // console.log(res);
 
     // if (res !== "") fpublish({ msg: res });
+  };
+
+  const TblRate = (props) => {
+    const { data, dmst, share, title } = props;
+
+    const titles = () => {
+      let h = knt === kntBulk ? "ГУРТ" : "РОЗДРІБ";
+      let f = "";
+      if (dmst === "2") {
+        // h += "";
+        f = "\n☎️ 096 001 36 00\n🌐 kantorfk.com";
+      } else if (dmst === "4") {
+        h += " ІНШІ ВАЛЮТИ";
+        f = "\n☎️ 096 001 36 00\n🌐 kantorfk.com";
+      } else if (dmst === "6") {
+        h += " КОНВЕРТАЦІЯ";
+        f = "\nКРОСКУРСИ ДЛЯ БІЛОГО ДОЛАРА\n🌐 kantorfk.com";
+      }
+      return { header: h, footer: f };
+    };
+    return (
+      data.filter((v) => v.domestic === dmst).length !== 0 && (
+        <TableContainer component={Paper} variant={"outlined"}>
+          <Table size="small" aria-label="a dense table">
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  {share && (
+                    <Tooltip title="Поширити" variant="soft">
+                      <IconButton
+                        size="small"
+                        color="primary"
+                        // disabled={!share}
+                        onClick={() =>
+                          fpublish(
+                            data.filter((v) => v.domestic === dmst),
+                            titles()
+                          )
+                        }
+                      >
+                        <ShareIcon />
+                      </IconButton>
+                    </Tooltip>
+                  )}
+                  {/* <Typography color={grey[500]}>name</Typography> */}
+                </TableCell>
+                <TableCell align="center">
+                  <Typography color={grey[500]}>bid</Typography>
+                </TableCell>
+                <TableCell align="right">
+                  <Typography color={grey[500]}>ask</Typography>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data
+                .filter((v) => v.domestic === dmst)
+                .map((v) => {
+                  return (
+                    <TableRow
+                      id={`id-${v.chid}-${v.shop}-${v.scode}-${v.prc}`}
+                      key={`key-${v.chid}-${v.shop}-${v.scode}-${v.prc}`}
+                      sx={{
+                        "&:last-child td, &:last-child th": { border: 0 },
+                      }}
+                    >
+                      <TableCell align="center">
+                        <Box
+                          sx={{
+                            display: "flex",
+                            alignItems: "center",
+                            gap: "0.5rem",
+                          }}
+                        >
+                          <Typography>{v.chid}</Typography>
+                          {knt === "" && (
+                            <Typography variant="caption">{v.shop}</Typography>
+                          )}
+                          <Typography color={grey[800]} variant="caption">
+                            {v.sname}
+                          </Typography>
+                          {Number(v.rqty) !== 1 && (
+                            <Typography variant="caption">{v.rqty}</Typography>
+                          )}
+                        </Box>
+                      </TableCell>
+                      <TableCell align="right">
+                        <Typography color={grey[800]}>
+                          {!isNaN(v.bid)
+                            ? Number(v.bid) !== 0
+                              ? Number(v.bid).toPrecision(4)
+                              : ""
+                            : v.bid}
+                        </Typography>
+                      </TableCell>
+                      <TableCell align="right">
+                        <Typography color={grey[800]}>
+                          {!isNaN(v.ask)
+                            ? Number(v.ask) !== 0
+                              ? Number(v.ask).toPrecision(4)
+                              : ""
+                            : v.ask}
+                        </Typography>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )
+    );
   };
 
   // useEffect(() => {
@@ -315,79 +438,30 @@ export const Rate = (props) => {
             </Table>
           </TableContainer>
         )}
-        <TableContainer component={Paper}>
-          <Table size="small" aria-label="a dense table">
-            <TableHead>
-              <TableRow>
-                <TableCell>
-                  <Typography color={grey[500]}>name</Typography>
-                </TableCell>
-                <TableCell align="center">
-                  <Typography color={grey[500]}>bid</Typography>
-                </TableCell>
-                <TableCell align="right">
-                  <Typography color={grey[500]}>ask</Typography>
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {sqldata
-                .filter(
-                  (v) => (knt === "" || knt === v.shop) && v.prc !== "bulk"
-                )
-                .map((v) => {
-                  return (
-                    <TableRow
-                      id={`id-${v.chid}-${v.shop}-${v.scode}-${v.prc}`}
-                      key={`key-${v.chid}-${v.shop}-${v.scode}-${v.prc}`}
-                      sx={{
-                        "&:last-child td, &:last-child th": { border: 0 },
-                      }}
-                    >
-                      <TableCell align="center">
-                        <Box
-                          sx={{
-                            display: "flex",
-                            alignItems: "center",
-                            gap: "0.5rem",
-                          }}
-                        >
-                          <Typography>{v.chid}</Typography>
-                          {knt === "" && (
-                            <Typography variant="caption">{v.shop}</Typography>
-                          )}
-                          <Typography color={grey[800]} variant="caption">
-                            {v.sname}
-                          </Typography>
-                          {Number(v.rqty) !== 1 && (
-                            <Typography variant="caption">{v.rqty}</Typography>
-                          )}
-                        </Box>
-                      </TableCell>
-                      <TableCell align="right">
-                        <Typography color={grey[800]}>
-                          {!isNaN(v.bid)
-                            ? Number(v.bid) !== 0
-                              ? Number(v.bid).toPrecision(4)
-                              : ""
-                            : v.bid}
-                        </Typography>
-                      </TableCell>
-                      <TableCell align="right">
-                        <Typography color={grey[800]}>
-                          {!isNaN(v.ask)
-                            ? Number(v.ask) !== 0
-                              ? Number(v.ask).toPrecision(4)
-                              : ""
-                            : v.ask}
-                        </Typography>
-                      </TableCell>
-                    </TableRow>
-                  );
-                })}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        <TblRate
+          data={sqldata.filter(
+            (v) => (knt === "" || knt === v.shop) && v.prc !== "bulk"
+          )}
+          dmst={"2"}
+          share={knt !== "" && pl.role === "owner"}
+          title={"TITLE"}
+        />
+        <TblRate
+          data={sqldata.filter(
+            (v) => (knt === "" || knt === v.shop) && v.prc !== "bulk"
+          )}
+          dmst={"4"}
+          share={false}
+          title={"TITLE"}
+        />
+        <TblRate
+          data={sqldata.filter(
+            (v) => (knt === "" || knt === v.shop) && v.prc !== "bulk"
+          )}
+          dmst={"6"}
+          share={knt !== "" && pl.role === "owner"}
+          title={"TITLE"}
+        />
         <TblCross data={crossRate(USDSUB)} title={"($100 білий)"} />
         <TblCross data={crossRate()} title={"($100 синій)"} />
       </Stack>
@@ -395,6 +469,7 @@ export const Rate = (props) => {
   );
 };
 
+// Table for cross rates
 const TblCross = (props) => {
   const { data, title } = props;
   return (
